1e750f387c2e946e5b401f6025e2c586b0d50df7
taranjeet <taranjeetplay@gmail.com>
2016-11-03 22:41:58 +0530

    configured a little

--- app/Http/socket.js
+++ app/Http/socket.js
@@ -2,13 +2,21 @@ const http = require('http');
 const server = http.createServer();  
 const io = use('socket.io')(server);
 //creating a list of users
-const users = [];  
+const users = {};  
 io.on('connection', function(socket){
+      socket.on('register',function(name) {
+        users.name=socket.id;
+        console.log("socket id is "+users.name);
+      });
     //Sending message to Specific user
-      socket.on('message',function(data_server){
-      console.log("message is recieved "+data_server.id+" "+data_server.msg+" "+data_server.name);
-      socket.broadcast.to(data_server.id).emit({msg:data_server.msg,id:data_server.id,name:data_server.name});
+      socket.on('messag',function(data_server){
+        console.log("messag event triggered");
+        console.log("message is recieved " + data_server.id + " " + data_server.msg + " " + data_server.name);
+        //socket.broadcast.to(data_server.id).emit('message', data_server.msg);
+      socket.emit('message',data_server.msg);
     });
-});
+    // socket.on('messag', function(msg){
+    // io.emit('message', msg);
+  });
 
 io.listen(3000);
