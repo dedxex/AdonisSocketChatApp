0575a8575babd06819517be1c817499225628d95
taranjeet.s <taranjeet.s>
2016-11-04 12:19:07 +0530

    sending private message successfully done feeling good

--- app/Http/socket.js
+++ app/Http/socket.js
@@ -5,16 +5,23 @@ const io = use('socket.io')(server);
 const users = {};  
 io.on('connection', function(socket){
       socket.on('register',function(name) {
-        users.name=socket.id;
-        console.log("socket id is "+users.name);
+        users[name]=socket.id;
+        console.log("socket id of "+name+" is "+users[name]+" is registered");
       });
     //Sending message to Specific user
-      socket.on('messag',function(data_server){
+
+        socket.on('messag',function(data_server){
         console.log("messag event triggered");
-        console.log("message is recieved " + data_server.id + " " + data_server.msg + " " + data_server.name);
+        console.log("message"+data_server.msg+" is sent to " + data_server.id + ","  + " from" + data_server.name);
+        
         //socket.broadcast.to(data_server.id).emit('message', data_server.msg);
-      socket.emit('message',data_server.msg);
-    });
+        //socket.emit('message',data_server.msg);
+        const name = data_server.id;
+        const socketId = users[data_server.id];
+        console.log("preparing to send to socketId "+socketId+" of name "+name+" by "+data_server.name);
+        socket.broadcast.to(socketId).emit('message', data_server.msg);
+      });
+
     // socket.on('messag', function(msg){
     // io.emit('message', msg);
   });
